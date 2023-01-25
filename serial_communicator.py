dc863f55ea102b6cfe29e786c008879b95ceb226
captaincrazybro <myeyesareonmizar@gmail.com>
2023-01-25 10:39:28 -0500

    Fixed merge issue

--- serial_communicator.py
+++ serial_communicator.py
@@ -2,9 +2,9 @@ import string
 import serial
 from enum import Enum
 
-PORT = 'COM6'
+PORT = 'COM3'
 DIVIDER = "|"
-BAUDRATE = 19200
+BAUDRATE = 9600
 
 class SerialCommunicator:
     def __init__(self):
@@ -12,11 +12,11 @@ class SerialCommunicator:
 
     def write(self, msg: str):
         self.arduino.write(f'{msg}{DIVIDER}')
+        print(f'{msg}{DIVIDER}')
     
-    def write_motor_command(self, motor_code: str, motor_value: int):
-        self.write(f'{motor_code}{motor_value}')
+    def write_motor_command(self, left_motor_code: str, right_motor_code: str, left_motor_value: int, right_motor_value: int):
+        self.write(f'{left_motor_code}{left_motor_value}{right_motor_code}{right_motor_value}')
 
 class MotorCode(Enum):
-    RIGHT = "rw"
-    LEFT = "lw"
-    ALL = "aw"
+    WHEELS_FORWARD = "wf"
+    WHEELS_BACKWARD = "wb"
